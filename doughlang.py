1d4436b1e0397ad098f291d348b81aec6315216a
Piotr Fila <piofila2000@gmail.com>
2021-01-27 21:26:10 +0100

    Better color support
    Added suppor for hex-formatted colors

--- doughlang.py
+++ doughlang.py
@@ -5,30 +5,39 @@ import os
 import discord
 from discord.ext import commands
 
-description = '''This bot can write doughlang'''
-
 intents = discord.Intents.default()
 
-bot = commands.Bot(command_prefix='?', description=description, intents=intents)
+bot = commands.Bot(command_prefix='?', description='''This bot can write doughlang''', intents=intents)
 
 glyphs = (
-        { "name":"Augustus", "offset":(0, 0) },
-        { "name":"Tiberius", "offset":(0, 0) },
-        { "name":"Gaius", "offset":(0, 0) },
-        { "name":"Claudius", "offset":(72, 0) },
-        { "name":"Nero", "offset":(72, 0) },
-        { "name":"Bob", "offset":(144, 0) },
-        { "name":"Otho", "offset":(0, 0) },
-        { "name":"Vitellius", "offset":(0, 0) },
-        { "name":"Hadrian", "offset":(0, 0) },
-        { "name":"Domitian", "offset":(72, 0) },
-    )
+    { "name":"Augustus", "offset":(0, 0) },
+    { "name":"Tiberius", "offset":(0, 0) },
+    { "name":"Gaius", "offset":(0, 0) },
+    { "name":"Claudius", "offset":(72, 0) },
+    { "name":"Nero", "offset":(72, 0) },
+    { "name":"Bob", "offset":(144, 0) },
+    { "name":"Otho", "offset":(0, 0) },
+    { "name":"Vitellius", "offset":(0, 0) },
+    { "name":"Hadrian", "offset":(0, 0) },
+    { "name":"Domitian", "offset":(72, 0) },
+)
 glyphmap = {}
-def_color_glyph = (180, 196, 104)
-def_color_backg = (0, 0, 0, 0)
+def_color_glyph = "888888"
+def_color_special = "#cc1111"
+def_color_backg = "#00000000"
 bitwise_to_letters = ()
 
-def makeimg(blocks, color_glyph, color_backg):
+def resolve_color(hx, a = 255):
+    if hx[:1] == "#":
+        hx = hx[1:]
+    r = int(hx[0:2], 16)
+    g = int(hx[2:4], 16)
+    b = int(hx[4:6], 16)
+    if (len(hx) > 6):
+        a = int(hx[6:8], 16)
+    return (r, g, b, a)
+
+def makeimg(blocks, color_glyph, color_special, color_backg):
     block_count = [1, 0]
     block_offset = (240, 100) # defined experimentally pretty much
     padding = (10, 10)
@@ -48,12 +57,12 @@ def makeimg(blocks, color_glyph, color_backg):
         padding[1] + block_count[1] * block_offset[1] + 55), color_backg)
     
     colored_img = [g["img"] for g in glyphs]
-    for n in colored_img:
-        pixels = n.load()
-        for i in range(n.size[0]): # for every pixel in every image
-            for j in range(n.size[1]):
+    for n in range(0, len(colored_img)):
+        pixels = colored_img[n].load()
+        for i in range(colored_img[n].size[0]): # for every pixel in every image
+            for j in range(colored_img[n].size[1]):
                 if pixels[i,j][3] == 255: # if the pixel is opaque, change it to color_glyph
-                    pixels[i,j] = color_glyph
+                    pixels[i,j] = color_glyph if n < 8 else color_special
     
     offset = list(padding)
     for b in blocks:
@@ -82,17 +91,17 @@ async def on_ready():
     print('------')
 
 @bot.command(description='makes a sentence')
-async def dl(ctx, text: str, r=def_color_glyph[0], g=def_color_glyph[1], b=def_color_glyph[2]):
+async def dl(ctx, text: str, color_glyph=def_color_glyph, color_special=def_color_special, color_backg=def_color_backg):
     if text == "e" or text == "E":
         await ctx.send("https://i.kym-cdn.com/photos/images/newsfeed/001/365/818/183.jpg")
     else:
-        await ctx.send(file=makeimg(text.split('/'), (r, g, b, 255), def_color_backg))
+        await ctx.send(file=makeimg(text.split('/'), resolve_color(color_glyph), resolve_color(color_special), resolve_color(color_backg)))
 
 @bot.command(description='makes a sentence asterisk syntax style')
-async def dla(ctx, text: str, r=def_color_glyph[0], g=def_color_glyph[1], b=def_color_glyph[2]):
+async def dla(ctx, text: str, color_glyph=def_color_glyph, color_special=def_color_special, color_backg=def_color_backg):
     # simply convert to the other format
     blocks = ["" if b == "" else bitwise_to_letters[int(b)] for b in text.split(',')]
-    await ctx.send(file=makeimg(blocks, def_color_glyph, def_color_backg))
+    await ctx.send(file=makeimg(blocks, resolve_color(color_glyph), resolve_color(color_special), resolve_color(color_backg)))
 
 if __name__ == "__main__":
     charset = Image.open("./charset.png")
