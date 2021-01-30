207662e8e010c14ad785dd3ce8175e4473e198bb
Piotr Fila <piofila2000@gmail.com>
2021-01-30 18:55:19 +0100

    Added mudular color support
    - Added mudular color support
    - (hopefully) Made some code easier to read

--- doughlang.py
+++ doughlang.py
@@ -7,93 +7,87 @@ import hashlib
 import requests
 import time
 import sys
-from PIL import Image
+from PIL import Image, ImageDraw
 import numpy as np
 import aiohttp
 import asyncio
 
 
-glyph_names = ()
-glyph_map_text = {}
-glyph_map_numbers = ()
-fonts = {}
+glyph_map = {}
+glyph_map_asterisk = ()
+modular_colors = {}
 color_pallettes = {}
-invalid_responses = ()
+fonts = {}
 default_font = ""
+invalid_sha_responses = ()
 developers = ()
 dev_mode = None
 
 
-def hex_to_color(hx, a = 255):
-    if hx[:1] == "#":
-        hx = hx[1:]
-    r = int(hx[0:2], 16)
-    g = int(hx[2:4], 16)
-    b = int(hx[4:6], 16)
-    if (len(hx) > 6):
-        a = int(hx[6:8], 16)
-    return r + (g << 8) + (b << 16) + (a << 24)
+def hex_to_color(hx): # returns the color of "#rrggbb" in uint32 form
+    r = int(hx[1:3], 16)
+    g = int(hx[3:5], 16)
+    b = int(hx[5:7], 16)
+    return r + (g << 8) + (b << 16) + 0xFF000000
 
+def resolve_pallette(name):
+    if name in color_pallettes:
+        return color_pallettes[name]
+    elif len(name.split("-")) == 2:
+        c1, c2 = name.split("-")
+        if c1 in modular_colors and c2 in modular_colors:
+            return ((modular_colors[c2], 0x300c0300), (modular_colors[c1], 0xff3fcff))
+    elif name in modular_colors:
+        return ((modular_colors[name], 0x3fffffff),)
+    return None
 
 def loadres(is_dev=None):
-    global glyph_names
-    global glyph_map_text
-    global glyph_map_numbers
-    global fonts
+    global glyph_map
+    global glyph_map_asterisk
+    global modular_colors
     global color_pallettes
-    global invalid_responses
+    global fonts
     global default_font
+    global invalid_sha_responses
     global developers
     global dev_mode
 
     with open("./config.json", "r") as f:
         obj = json.load(f)
     
-    glyph_names = tuple(obj["glyph-names"])
+    glyph_map = {}
+    for mapping in obj["glyph-mapping"]:
+        for i in range(10):
+            bits = 0b100000000010000000001 << i
+            glyph_map[mapping[i]] = bits
 
+    glyph_map_asterisk = tuple(i * 0b100000000010000000001 for i in range(1024))
+
+    modular_colors = {}
+    for name in obj["modular-colors"]:
+        modular_colors[name] = hex_to_color(obj["modular-colors"][name])
+    
     color_pallettes = {}
     for name in obj["color-pallettes"]:
-        pairs = [(hex_to_color(color), obj["color-pallettes"][name][color]) for color in obj["color-pallettes"][name]]
-        color_pallettes[name] = tuple(pairs)
+        color_pallettes[name] = tuple((hex_to_color(color_key), obj["color-pallettes"][name][color_key]) for color_key in obj["color-pallettes"][name])[::-1]
 
     fonts = {}
     for name in obj["fonts"]:
         fonts[name] = {}
-        ldict = obj["fonts"][name]
-        with open(ldict["source"], "rb") as f:
+        cfg_font = obj["fonts"][name]
+        with open(cfg_font["source"], "rb") as f:
             fonts[name]["arr"] = np.copy(np.swapaxes(np.load(f), 0, 1))
-        fonts[name]["block_offset"] = ldict["block-offset"]
-        fonts[name]["default_pallette"] = color_pallettes[ldict["default-pallette"]]
-
-    
-    glyph_map_numbers = []
-    for i in range(0, 1024):
-        glyph_map_numbers.append(0)
-        for n in range(0, 10):
-            mask = 0b100000000010000000001 << n
-            if i & mask != 0:
-                glyph_map_numbers[-1] |= mask
-    glyph_map_numbers = tuple(glyph_map_numbers)
-    
-    glyph_map_text = {}
-    for i in range(0, len(glyph_names)):
-        bits = 0b100000000010000000001 << i
-        glyph_map_text[glyph_names[i][:1].upper()] = bits
-        glyph_map_text[str((i + 1) % 10)] = bits
-    
-    for i in range(0, len(obj["bread-names"])):
-        bits = 0b100000000010000000001 << i
-        glyph_map_text[obj["bread-names"][i].lower()] = bits
+        fonts[name]["block_offset"] = cfg_font["block-offset"]
+        fonts[name]["default_pallette"] = resolve_pallette(cfg_font["default-pallette"])
 
     default_font = obj["default-font"]
-
+    invalid_sha_responses = tuple(obj["invalid-sha-responses"])
     developers = tuple(obj["developers"])
 
     # variables used to configure discord.py
-    botvars = {}
-    invalid_responses = tuple(obj["invalid-responses"])
-
     if is_dev is not None:
+        botvars = {}
+
         dev_mode = is_dev
         if is_dev:
             botvars_postfix = "-dev"
@@ -115,40 +109,27 @@ def loadres(is_dev=None):
 
 
 if __name__ == "__main__":
-    global bot
-    if "--dev" in sys.argv:
-        print("starting in developer mode")
-        botvars = loadres(True)
-    else:
-        botvars = loadres(False)
+    botvars = loadres("--dev" in sys.argv)
     bot = commands.Bot(command_prefix=botvars["prefix"], description=botvars["description"], intents=discord.Intents.default())
 
 # commands
 
 @bot.event
 async def on_ready():
-    print('Logged in as')
-    print(bot.user.name)
-    print(bot.user.id)
-    print('------')
+    print(f"Logged in as {bot.user.name} : {bot.user.id}")
 
 
 @bot.check
 async def dev_mode_check(ctx):
-    if dev_mode and ctx.author.id not in developers:
-        ctx.send("This is the dev bot. Use the regular bot instead.")
-        return False
-    else:
-        return True
+    return not dev_mode or ctx.author.id in developers
 
 
 class DrawIterator:
-    def __init__(self, blocks, fontarr, color_pallette, canvas_size, block_size, block_offset):
+    def __init__(self, blocks, fontarr, color_pallette, canvas_size, block_offset):
         self.blocks = blocks
         self.fontarr = fontarr
         self.color_pallette = color_pallette
         self.canvas_size = canvas_size
-        self.block_size = block_size
         self.block_offset = block_offset
         self.up_pass = []
         self.down_pass = []
@@ -158,11 +139,11 @@ class DrawIterator:
         return self
     
     def __next__(self):
-        if self.row == self.canvas_size[1]:
+        if self.row == self.canvas_size[1]: # reached the bottom of the image
             raise StopIteration
         else:
-            j = int(self.row / self.block_offset[1])
-            if self.row % self.block_offset[1] == self.block_size[1] - self.block_offset[1]:
+            j = int(self.row / self.block_offset[1]) # 
+            if self.row % self.block_offset[1] == self.fontarr.shape[0] - self.block_offset[1]:
                 self.up_pass = []
                 
             elif self.row % self.block_offset[1] == 0:
@@ -171,13 +152,13 @@ class DrawIterator:
                 self.down_pass = []
                 for col in self.blocks:
                     if len(col) > j:
-                        npass = []
+                        new_pass = []
                         for _, cmask in self.color_pallette:
                             mask = cmask & col[j]
-                            if mask != 0:
-                                npass.append(mask)
-                        if len(npass) > 0:
-                            self.down_pass.append(npass)
+                            if mask:
+                                new_pass.append(mask)
+                        if new_pass:
+                            self.down_pass.append(new_pass)
                     else:
                         self.down_pass.append([])
             
@@ -192,9 +173,7 @@ class DrawIterator:
             
             result = np.zeros(buffer.size, dtype=np.uint32)
             for color, mask in self.color_pallette:
-                temp = (buffer & mask)
-                temp[temp!=0] = color
-                result |= temp
+                result[((buffer & mask)!=0)&(result==0)] = color
 
             self.row+=1
             return result
@@ -205,16 +184,28 @@ async def dl(ctx, text: str, font=None, color_pallette=None):
     begin = time.time()
 
     # chose font and color pallette
-    if font is None:
-        font = fonts[default_font]
+    if font is not None:
+        if font in fonts:
+            font = fonts[font]
+        else:
+            color_pallette = resolve_pallette(font)
+            if color_pallette is not None:
+                color_pallette=font
+                font=fonts[default_font]
+            else:
+                await ctx.send("Invalid font.")
+                return
     else:
-        font = fonts[font]
+        font = fonts[default_font]
+    
     if color_pallette is not None:
-        color_pallette = color_pallettes[color_pallette]
+        color_pallette = resolve_pallette(color_pallette)
+        if color_pallette is None:
+            await ctx.send("Invalid color palette.")
+            return
     else:
         color_pallette = font["default_pallette"]
     
-    print(len(glyph_map_numbers))
     if "," in text:
         # asterisk encoding
         blocks = [[]]
@@ -224,20 +215,19 @@ async def dl(ctx, text: str, font=None, color_pallette=None):
             else:
                 modif = 0
                 newl = len(blk)
-                if blk.endswith("//"):
+                if blk.endswith(r"//"):
                     modif += 256
                     newl = -2
-                    if blk.endswith("\\\\//"):
+                    if blk.endswith(r"\\//"):
                         modif += 512
                         newl = -4
-                elif blk.endswith("\\"):
+                elif blk.endswith(r"\\"):
                     modif += 512
                     newl = -2
-                    if blk.endswith("//\\\\"):
+                    if blk.endswith(r"//\\"):
                         modif += 256
                         newl = -4
-                print(modif)
-                blocks[-1].append(glyph_map_numbers[int(blk[:newl]) + modif])
+                blocks[-1].append(glyph_map_asterisk[int(blk[:newl]) + modif])
     else:
         blocks = []
         text = text.replace("//", " ")
@@ -247,15 +237,14 @@ async def dl(ctx, text: str, font=None, color_pallette=None):
             for blk in column.split("-"):
                 charmask = 0
                 for g in blk:
-                    charmask |= glyph_map_text[g]
+                    charmask |= glyph_map[g]
                 blocks[-1].append(charmask)
     
     grid_dimensions = (len(blocks), max([len(i) for i in blocks]))
     
-    real_shape = [font["arr"].shape[(i+1)%2] for i in (0,1)]
-    img_size = [font["block_offset"][i] * (grid_dimensions[i] - 1) + real_shape[i] for i in (0,1)]
+    img_size = [font["block_offset"][i] * (grid_dimensions[i] - 1) + font["arr"].shape[(i+1)%2] for i in (0,1)]
 
-    row_iterator = DrawIterator(blocks, font["arr"], color_pallette, img_size, real_shape, font["block_offset"])
+    row_iterator = DrawIterator(blocks, font["arr"], color_pallette, img_size, font["block_offset"])
     arr = io.BytesIO()
     Image.fromarray(np.vstack(tuple(row_iterator)), "RGBA").save(arr, "png")
     arr.seek(0)
@@ -278,6 +267,38 @@ async def sha(ctx, text: str):
                 response = f"BAD {response.status}"
     
     await ctx.send(f"{response} {link}")
+
+
+@bot.command(name="fonts")
+async def _fonts(ctx):
+    response = "```Available fonts:"
+    for name in fonts:
+        response += f"\n{name}"
+    await ctx.send(response + "```")
+
+@bot.command()
+async def colors(ctx):
+    def color_hex(num):
+        r, g, b = [(num >> i)&0xFF for i in(0,8,16)]
+        return f"#{r:0{2}x}{g:0{2}x}{b:0{2}x}"
+    
+    response = "```Available colors:"
+    cols = [n for n in modular_colors]
+    
+    for i in range(0, len(modular_colors), 4):
+        response+=f"\n{cols[i]} {cols[i+1]} {cols[i+2]} {cols[i+3]}"
+    # draw a grid of available colors
+    w, h = 40, 40
+    img = Image.new("RGB", (w * 4, h * 4))
+    imgd = ImageDraw.Draw(img)
+    for x in range(4):
+        for y in range(4):
+            imgd.rectangle(((x*w, y*h), (x*w+w, y*h+h)), fill=color_hex(modular_colors[cols[x + y * 4]]))
+    
+    arr = io.BytesIO()
+    img.save(arr, "png")
+    arr.seek(0)
+    await ctx.send(response + "```", file=discord.File(arr, "colors.png"))
 
 
 @bot.command()
